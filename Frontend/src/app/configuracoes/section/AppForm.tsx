1b3496c2ab88fc6190c98a5d55d7cefd92e6ba5e
Gabriel de Oliveira <148984768+GabriD-0@users.noreply.github.com>
2025-06-07 14:08:57 -0300

    Melhoria visual da tela de configuração

--- Frontend/src/app/configuracoes/section/AppForm.tsx
+++ Frontend/src/app/configuracoes/section/AppForm.tsx
@@ -5,7 +5,7 @@ import { useSaveConfig } from '../hooks/useSaveConfig';
 import { AppSettings } from '../../types/app-settings';
 
 function AppForm() {
-  const { control, register, handleSubmit } = useForm<AppSettings>({
+  const { register, handleSubmit, control } = useForm<AppSettings>({
     defaultValues: { 
       tema: 'system',
       lingua: 'pt-BR',
@@ -15,55 +15,71 @@ function AppForm() {
   });
 
   const { save, loading } = useSaveConfig<AppSettings>('app');
-
-  const onSubmit = (data: AppSettings) =>  save(data);
+  const onSubmit = (data: AppSettings) => save(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}
-      className="rounded-xl border bg-white p-6 shadow-sm ">
+      className="bg-white dark:bg-stroke-700 rounded-2xl shadow p-6">
 
-      <h2 className="mb-4 text-lg font-semibold">
+      <h2 className="mb-4 text-2xl font-semibold">
         Sistema
       </h2>
 
-      <div className="mb-4">
+      <div className="mb-4 text-md text-gray-900">
         <label className="label">
-          Tema
+          Tema:
         </label>
 
-        <select className="select ml-1" {...register('tema')}>
+        <select className="mt-1 block w-full rounded-md border border-gray-300 bg-white  text-gray-900 shadow-sm focus:ring-2 focus:outline-none h-7"
+         {...register('tema')}>
           <option value="light">Claro</option>
           <option value="dark">Escuro</option>
           <option value="system">Sistema</option>
         </select>
       </div>
 
-      <div className="mb-4">
+      <div className="mb-4 text-md text-gray-900">
         <label className="label">
-          Idioma
+          Idioma:
         </label>
         
-        <select className="select ml-1" {...register('lingua')}>
+        <select className="mt-1 block w-full rounded-md border border-gray-300 bg-white  text-gray-900 shadow-sm focus:ring-2 focus:outline-none h-7"
+        {...register('lingua')}>
           <option value="pt-BR">Português (BR)</option>
           <option value="en-US">English (US)</option>
         </select>
       </div>
 
-      <div className="mb-6">
+      <div className="mb-6 text-md text-gray-900">
         <label className="label">
-          Filial padraoFilial
+          Filial Padrão:
         </label>
-
         <input
-          className="input border ml-1 rounded-sm pl-1"
-          {...register('padraoFilial')}
-          placeholder="Ex.: Curitiba"/>
+            type="text"
+            className="mt-1 block w-full rounded-md border border-gray-300 bg-white text-gray-900 shadow focus:ring-2 focus:outline-none px-3 py-2"
+            placeholder="Ex.: Curitiba"
+            {...register('padraoFilial')}/>
       </div>
 
-      <button className="mt-22 btn-primary bg-gray-700/70 hover:bg-green-600/70 p-2 px-3 rounded-3xl text-white cursor-pointer "
-      disabled={loading}>
-        {loading ? 'Salvando…' : 'Salvar'}
-      </button>
+      <div className="md:col-span-2 flex items-center">
+        <input
+          type="checkbox"
+          id="emailNotification"
+          className="h-5 w-5 text-green-600 rounded border-gray-300  focus:ring-green-500"
+          {...register('emailNotification')}/>
+        <label htmlFor="emailNotification" className="ml-2 text-sm text-gray-700">
+          Receber notificações por email
+        </label>
+      </div>
+
+      <div className="lg:mt-26 flex justify-end">
+        <button
+          type="submit"
+          disabled={loading}
+          className="inline-flex items-center px-6 py-2 rounded-full bg-gray-600 hover:bg-green-700 text-white font-medium transition cursor-pointer">
+          {loading ? 'Salvando…' : 'Salvar'}
+        </button>
+      </div>
     </form>
   );
 }
--- Frontend/src/app/configuracoes/section/UsuarioForm.tsx
+++ Frontend/src/app/configuracoes/section/UsuarioForm.tsx
@@ -8,27 +8,26 @@ import { useSaveConfig } from '../hooks/useSaveConfig';
 import { UsuarioFormData, usuarioSchema } from '../../types/usuario';
 
 export default function UsuarioForm() {
-  const { register, handleSubmit, formState } = useForm<UsuarioFormData>({
+  const {
+  register,
+  handleSubmit,
+  formState: { errors },
+  } = useForm<UsuarioFormData>({
     resolver: zodResolver(usuarioSchema),
   });
 
   const { save, loading } = useSaveConfig<UsuarioFormData>('user');
   const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
 
-  function onSubmit(data: UsuarioFormData) {
-    save(data);
-  }
+  const onSubmit = (data: UsuarioFormData) => save(data);
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="rounded-xl border bg-white p-6 shadow-sm">
+      className="bg-white dark:bg-stroke-700 rounded-2xl shadow p-6">
+      <h2 className="mb-4 text-2xl font-semibold">Conta</h2>
 
-      <h2 className="mb-4 text-lg font-semibold">
-        Conta
-      </h2>
-
-      <div className="mb-5 flex items-center gap-4">
+      <div className="mb-6 flex items-center gap-4">
         <input
           id="avatar"
           type="file"
@@ -38,83 +37,92 @@ export default function UsuarioForm() {
           onChange={(e) => {
             const file = e.target.files?.[0];
             if (file) setAvatarPreview(URL.createObjectURL(file));
-          }}/>
+          }}
+        />
 
-        <label htmlFor="avatar"
-          className="relative cursor-pointer group">
-
+        <label htmlFor="avatar" className="relative cursor-pointer group">
           {avatarPreview ? (
             <img
               src={avatarPreview}
               alt="avatar"
-              className="h-16 w-16 rounded-full object-cover ring-2 ring-amber-500"/>
+              className="h-16 w-16 rounded-full object-cover ring-2"/>
           ) : (
             <UserCircleIcon className="h-16 w-16 text-gray-400" />
           )}
 
-          <div className="absolute inset-0 flex items-center 
-          justify-center rounded-full bg-black/50 opacity-0 
-          transition-opacity group-hover:opacity-100">
-
+          <div className="absolute inset-0 flex items-center justify-center
+                       rounded-full bg-black/50 opacity-0 transition-opacity
+                       group-hover:opacity-100">
             <span className="text-xs font-medium text-white">
               Clique aqui
             </span>
           </div>
         </label>
 
-        <span className="text-md">
-          Selecione sua foto
-        </span>
+        <span className="text-md text-gray-900">Selecione sua foto</span>
       </div>
 
-      <div className="mb-4">
-        <label className="label">
-          Nome completo:
-        </label>
-
-        <input className="input border ml-1 rounded-sm pl-1"
-          {...register('nomeCompleto')}
-          placeholder="Seu nome"/>
-
-        {formState.errors.nomeCompleto && (
-          <p className="error text-red-600/80 ml-32">{formState.errors.nomeCompleto.message}</p>
-        )}
-      </div>
-
-      <div className="mb-4">
-        <label className="label">
-          Email:
-        </label>
-
-        <input className="input border ml-1 rounded-sm pl-1 w-66"
-          type="email"
-          {...register('email')}
-          placeholder="exemplo@email.com"/>
-
-        {formState.errors.email && (
-          <p className="error text-red-600/80 ml-13">{formState.errors.email.message}</p>
-        )}
-      </div>
-
-      <div className="mb-6">
-        <label className="label">
-          Nova senha:
+      <div className="mb-4 text-md text-gray-900">
+        <label htmlFor="nomeCompleto" className="label">
+          Nome completo
         </label>
         <input
-          className="input border ml-1 rounded-sm pl-1 w-54"
-          type="password"
-          {...register('senha')}
-          placeholder="********"/>
-
-        {formState.errors.senha && (
-          <p className="error text-red-600/80 ml-24">{formState.errors.senha.message}</p>
+          id="nomeCompleto"
+          type="text"
+          placeholder="Seu nome"
+          className="mt-1 block w-full rounded-md border border-gray-300
+                     bg-white text-gray-900 shadow focus:ring-2 focus:outline-none
+                     px-3 py-2"
+          {...register('nomeCompleto')}/>
+        {errors.nomeCompleto && (
+          <p className="mt-1 text-sm text-red-600">{errors.nomeCompleto.message}</p>
         )}
       </div>
 
-      <button className="btn-primary bg-gray-700/70 hover:bg-green-600/70 p-2 px-3 rounded-3xl text-white cursor-pointer "
-      disabled={loading}>
-        {loading ? 'Salvando…' : 'Salvar'}
-      </button>
+      <div className="mb-4 text-md text-gray-900">
+        <label htmlFor="email" className="label">
+          Email
+        </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="exemplo@email.com"
+          className="mt-1 block w-full rounded-md border border-gray-300
+                     bg-white text-gray-900 shadow focus:ring-2 focus:outline-none
+                     px-3 py-2"
+          {...register('email')}/>
+        {errors.email && (
+          <p className="mt-1 text-sm text-red-600">{errors.email.message}</p>
+        )}
+      </div>
+
+      <div className="mb-4 text-md text-gray-900">
+        <label htmlFor="senha" className="label">
+          Nova senha
+        </label>
+        <input
+          id="senha"
+          type="password"
+          placeholder="********"
+          className="mt-1 block w-full rounded-md border border-gray-300
+                     bg-white text-gray-900 shadow focus:ring-2 focus:outline-none
+                     px-3 py-2"
+          {...register('senha')}/>
+        {errors.senha && (
+          <p className="mt-1 text-sm text-red-600">{errors.senha.message}</p>
+        )}
+      </div>
+
+      <div className="mt-8 flex justify-end">
+        <button
+          type="submit"
+          disabled={loading}
+          className="inline-flex items-center px-6 py-2 rounded-full
+                     bg-gray-600 hover:bg-green-700 text-white font-medium
+                     transition cursor-pointer disabled:opacity-50">
+          {loading ? 'Salvando…' : 'Salvar'}
+        </button>
+      </div>
     </form>
   );
 }
